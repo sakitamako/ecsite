02407d051c4d8d61d3d9debbe3db24e80a4cfcf0
sakitamako <sa.919.1025.ki@gmail.com>
2024-07-17 21:24:00 +0900

    struts.xmlとUserCreateAction.java追加

---
+++ src/com/diworksdev/ecsite/action/UserCreateAction.java
@@ -0,0 +1,15 @@
+package com.diworksdev.ecsite.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//ユーザー登録機能
+
+
+public class UserCreateAction extends ActionSupport {
+
+	public String execute() {
+
+		return SUCCESS;
+	}
+
+}
