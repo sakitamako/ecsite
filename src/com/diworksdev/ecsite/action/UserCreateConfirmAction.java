02c64ef11ff43eebac4eba0fa9622028945c225b
sakitamako <sa.919.1025.ki@gmail.com>
2024-07-17 21:48:05 +0900

    UserCreateConfirmActionエラー出たけど一旦保存

---
+++ src/com/diworksdev/ecsite/action/UserCreateConfirmAction.java
@@ -0,0 +1,63 @@
+package com.diworksdev.ecsite.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//ユーザー登録機能
+
+public class UserCreateConfirmAction extends ActionSupport implements SessionAware {
+
+	private String loginUserId;
+	private String loginPassword;
+	private String userName;
+
+	public Map<String, Object> session;
+
+	private String errorMessage;
+
+	public String execute() {
+
+		String result = SUCCESS;
+
+		if (!(loginUserId.equals("")) && !(loginPassword .equals("")) && !(userName.equals(""))) {
+
+			session.put("loginUserId", loginUserId);
+			session.put("loginPassword", loginPassword);
+			session.put("userName", userName);
+
+		} else {
+
+			setErrorMessage("未入力の項目があります。");
+
+			result = ERROR;
+
+		}
+
+		return result;
+
+	}
+
+	public String getLoginUserId() {
+		return loginUserId;
+
+	}
+
+	public void setLoginUserId(String loginUserId) {
+		this.loginUserId = loginUserId;
+
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+
+	}
+
+}
