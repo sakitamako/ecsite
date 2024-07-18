3963cfd58974fd243d28c1a68db9328f98dbf619
sakitamako <sa.919.1025.ki@gmail.com>
2024-07-18 20:34:02 +0900

    UserCreateCompleteDAO.java追加

---
+++ src/com/diworksdev/ecsite/dao/UserCreateCompleteDAO.java
@@ -0,0 +1,39 @@
+package com.diworksdev.ecsite.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.diworksdev.ecsite.util.DBConnector;
+import com.diworksdev.ecsite.util.DateUtil;
+
+public class UserCreateCompleteDAO {
+
+	private DBConnector dbConnector = new DBConnector();
+	private Connection connection = dbConnector.getConnection();
+	private DateUtil dateUtil = new DateUtil();
+	private String sql = "INSERT INTO login_user_transaction (login_id, login_pass, user_name, insert_date) VALUES(?, ?, ?, ?)";
+
+	public void cerateUser(String loginUserId, String loginUserPassword, String userName) throws SQLException {
+
+		try {
+
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+			preparedStatement.setString(1, loginUserId);
+			preparedStatement.setString(2, loginUserPassword);
+			preparedStatement.setString(3, userName);
+			preparedStatement.setString(4, dateUtil.getDate());
+			preparedStatement.execute();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			connection.close();
+
+		}
+
+	}
+
+}
