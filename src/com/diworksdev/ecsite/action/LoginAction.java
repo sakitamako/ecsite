0562a9e851124d6ed6c573b4b2512e34a0fcb460
sakitamako <sa.919.1025.ki@gmail.com>
2024-07-22 22:24:37 +0900

    Loginファイルのコメント追加中

--- src/com/diworksdev/ecsite/action/LoginAction.java
+++ src/com/diworksdev/ecsite/action/LoginAction.java
@@ -10,65 +10,119 @@ import com.diworksdev.ecsite.dto.BuyItemDTO;
 import com.diworksdev.ecsite.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
+//ログイン認証機能
+
+//Actionクラスでは、画面から送られてきたリクエストを取得する
+//内部処理に応じてDAOやDTOクラスを呼び出し、最終的に次のJSPへ値を返すファイル
+
+//struts2が持つActionSupportというクラスを継承
+//（Actionクラスは基本的にこのクラスを継承）
+//LoginAciton（子クラス） extends（継承） ActionSupport（親クラス）
+//すでにあるクラスとにたクラスを作る場合、元のクラスに必要な機能だけを追加する形で、新しいクラスを作ることを継承
+//実際の処理を持たない、ちょっと変わったクラス=implements
+//interfaceを使って型宣言を行うことができますが、メソッドの定義がないとプログラムは実行できないので、そこで使うのがimplements
+/*Actionクラスにて、implements SessionAware を宣言（ActionSupport.SessionAware=インターフェース）
+実装メソッドである setSession(Map session)にて、ActionのフィールドへHttpSessionのオブジェクトを格納する処理を実装する。this.session = session; でほぼ十分。
+上記の手順で実装したフィールドを用意する
+これにより、このActionクラスのsessionフィールドへ、Struts2が自動的にHttpSessionの内容をMapの型で格納します。*/
 public class LoginAction extends ActionSupport implements SessionAware {
 
+	//フィールド変数
+	//JSPから受け取る値
+	//※必ずJSPでの定義と同じ名前にする
 	private String loginUserId;
 	private String loginPassword;
 
+	//Map<String, Object>=キーを値にマッピングするオブジェクト。
+	//マップには、同一のキーを複数登録できない。各キーは1つの値にしかマッピングできません。
+    //このインタフェースは、インタフェースというよりむしろ完全に抽象クラスであったDictionaryクラスに代わるものです
+	//全てのクラス 変数 変数名
 	public Map<String, Object> session;
 
+	//②LoginDAOとLoginDTOとbuyItemDAOのインスタンス化（コピーして代入）
 	private LoginDAO loginDAO = new LoginDAO();
 	private LoginDTO loginDTO = new LoginDTO();
 	private BuyItemDAO buyItemDAO = new BuyItemDAO();
 
+	//メソッド名は「execute」
+	//管理コマンド・メッセージをコマンド・サーバーに送信し、何らかの応答メッセージを待ちます
 	public String execute() {
 
+		//変数・文字列 result=ERROR
+		//初期値
 		String result = ERROR;
 
+		//JSPから送られてきた情報を引数として、
+		//LoginDAOクラスのgetLoginUserInfoメソッドを呼び出す
+		//その後、DAOで取得した結果をLoginDTOに代入する
 		loginDTO = loginDAO.getLoginUserInfo(loginUserId, loginPassword);
 
+		//Map を使った場合には、put()で要素を記憶できる
+		//sessionに記憶する
 		session.put("loginUser", loginDTO);
 
 		//入力値からユーザー情報の検索を行います。
-		//ログイン認証が成功した場合、次の画面で「商品情報」が必要なため商品情報を取得します。
+		//ログイン認証が成功した場合、次の画面で
+		//「商品情報」が必要なため商品情報を取得します。
+		//もしLoginDTOの中に入っているsessionからloginUserとgetLoginFlg()を取得した場合success
 		if (((LoginDTO)session.get("loginUser")).getLoginFlg()) {
 
 			result = SUCCESS;
 
+			//buyItemDTOファイルの  行目
+			//buyItemDAOクラスのgetBuyItemInfoメソッドを呼び出す
+			//その後、DAOで取得した結果をbuyItemDTOに代入する
 			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
 
+			//Map を使った場合には、put()で要素を記憶できる
+			//sessionの中に記憶した要素をそれぞれのDTOファイルから取得する
 			session.put("login_user_id", loginDTO.getLoginId());
 			session.put("id", buyItemDTO.getId());
 			session.put("buyItem_name", buyItemDTO.getItemName());
 			session.put("buyItem_price", buyItemDTO.getItemPrice());
 
+			//戻り値
+			//resultに入った値を呼び出し元であるActionクラスに渡す
 			return result;
 
 		}
 
+		//戻り値
+		//resultに入った値を呼び出し元であるActionクラスに渡す
 		return result;
 
 	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginUserIdフィールドの値をActionに渡す
 	public String getLoginUserId() {
 		return loginUserId;
 
 	}
 
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginUserIdフィールドに格納
 	public void setLoginUserId(String loginUserId) {
 		this.loginUserId = loginUserId;
 
 	}
 
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginPasswordフィールドの値をActionに渡す
 	public String getLoginPassword() {
 		return loginPassword;
 
 	}
 
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginPasswordフィールドに格納
 	public void setLoginPassword(String loginPassword) {
 		this.loginPassword = loginPassword;
 
 	}
 
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のsessionフィールドに格納
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
--- src/com/diworksdev/ecsite/dao/LoginDAO.java
+++ src/com/diworksdev/ecsite/dao/LoginDAO.java
@@ -7,45 +7,84 @@ import java.sql.ResultSet;
 import com.diworksdev.ecsite.dto.LoginDTO;
 import com.diworksdev.ecsite.util.DBConnector;
 
+//ログイン認証機能
+
+//DAOクラスでは、Actionから送られてきた情報を使ってDBへ問い合わせを行うファイル
+//問い合わせて取得した値をDTOクラスに格納するファイル
 public class LoginDAO {
 
+	//②DBConnectorのインスタンス化
+	//DBへの接続準備、DBと会話するためのコード、これでログインできる
+	//Connectionは特定のデータベースとの接続
 	private DBConnector dbConnector = new DBConnector();
+
+	//③getConnectionの呼び出し（DBと接続する）
 	private Connection connection = dbConnector.getConnection();
+
+	//LoginDTOインスタンス化
+	//DTOと会話するためのコード
 	private LoginDTO loginDTO = new LoginDTO();
 
+	//①クラス、メソッドの定義
+	//DTO型を最後に呼び出し元に渡すので、DTO型を戻り値にしたメソッドを作る
+	//Actionクラスの値を引数として受け取る
 	public LoginDTO getLoginUserInfo(String loginUserId, String loginPassword) {
 
+		//④sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため）
+		//SELECT データを抽出する
+		//＊ テーブルに含まれる項目全て
+		//FROM 〇〇 〇〇という名前のテーブルからデータを選択する
+		//WHERE ＜条件＞抽出条件を指定
+		//login_user_transactionに入っているデータid=? pass=?に入る条件を満たしたデータがsqlに代入される
 		String sql = "SELECT * FROM login_user_transaction where login_id=? AND login_pass=?";
 
+		//try.catchはjavaの例外処理のための構文
 		try {
 
+			//tryの中にはエラーが発生しそうな処理を書く
+			//⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
+			//定義したSQL文の1番目の?にActionから送られたname、
+			//2番目の?にActionから送られたpasswordがそれぞれ入る
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
+			//⑥sql文の?に入れる値をsetする
+			//セキュリティを考慮し、javaではPreparedStatementを利用
 			preparedStatement.setString(1, loginUserId);
 			preparedStatement.setString(2, loginPassword);
 
+			//⑦executeQuery()/executeUpdate()で実行
+			//sql文の値をセットしたものがresultsetに入ってる
 			ResultSet resultSet = preparedStatement.executeQuery();
 
+			//下に1行ずらすこと
+			//データが存在していれば戻り値を true で返す。存在しなければ falseで返す
 			if (resultSet.next()) {
 
+				//もしresultsetに入っている値が存在していればDBから取得した情報をDTOに格納する
 				loginDTO.setLoginId(resultSet.getString("login_id"));
 				loginDTO.setLoginPassword(resultSet.getString("login_pass"));
 				loginDTO.setUserName(resultSet.getString("user_name"));
 
+				//もしresultsetに入っている値(“login_id”) と nullが等しくない場合
+				//DBに保管されているIDとresultsetに入っているIDが等しくない場合はtrue!
 				if (resultSet.getString("login_id") != null) {
 
+					//DTOにtrueを格納する
 					loginDTO.setLoginFlg(true);
 
 				}
 
 			}
 
-
+		//処理中にSQL関連のエラーが発生した際に実行する処理
+		//tryの中でエラーが発生した場合、catchが受け取り
+		//例外がスローされる原因となったエラーまたは動作の説明を返す
 		} catch (Exception e) {
 			e.printStackTrace();
 
 		}
 
+		//dtoに入った値を呼び出し元であるアクションクラスに渡す
 		return loginDTO;
 
 	}
